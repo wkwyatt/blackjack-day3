b09e1cc4d5711a7768defd5a1c3a5a0fe2438d85
wkwyatt <wkwyatt@gmail.com>
2015-10-14 17:29:37 -0400

    BlackJack Game HTML & CSS

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var deck = [];
+
+function deal() {
+	// var deck = [];
+	var suit;
+	for(s = 1;s <= 4;s++) {
+		if(s === 1) {
+			suit = "H";
+		} else if(s === 2) {
+			suit = "S";
+		} else if(s === 3) {
+			suit = "D";
+		} else if(s === 4) {
+			suit = "C";
+		}
+		for(i = 1;i <= 13; i++){
+			deck.push(i+suit);
+		}
+	}
+	console.log(deck);
+
+	var numberOfTimesToShuffle = Math.floor(Math.random() * 500 + 500);
+	// Shuffle the deck
+	for(i = 0;i < numberOfTimesToShuffle;i++) {
+		// Pick 2 random cards in the deck and switch them
+		var card1 = Math.floor(Math.random() * 52);
+		var card2 = Math.floor(Math.random() * 52);
+
+		var temp = deck[card2];
+		deck[card2] = deck[card1];
+		deck[card1] = temp;
+	}
+
+	console.log(deck);
+}
+
+function hit() {
+
+}
+
+function stand() {
+
+}
